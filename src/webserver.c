23723e5114cf0ba26f501ab9d20a19103c8c4b60
gabo539 <gabrielporebski@proton.me>
2023-11-20 16:28:23 +0100

    WIP: 2.7

--- src/webserver.c
+++ src/webserver.c
@@ -130,7 +130,26 @@ int webserver_tick(webserver *ws) {
 
                         } else res->header->status_code = 404;
 
-                    } else res->header->status_code = 501;
+                    } else if (strncmp(req->header->method, "PUT", 3) == 0) {
+
+                        if (strncmp(req->header->URI, "dynamic", 7) != 0) {
+                            res->header->status_code = 403;
+                            strcpy(res->header->status_message, "Forbidden");
+                        } else if {
+
+                        }
+
+                    } else if (strncmp(req->header->method, "DELETE", 3) == 0) {
+
+                        if (strncmp(req->header->URI, "dynamic", 7) != 0) {
+                            res->header->status_code = 403;
+                            strcpy(res->header->status_message, "Forbidden");
+                        } else {
+
+                        }
+                    } else {
+                        res->header->status_code = 501;
+                    }
 
                 } else res->header->status_code = 400;
 
