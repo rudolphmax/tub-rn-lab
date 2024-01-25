26c68001810e5db639553ca8ddd9a954e4c1f2d2
rudolphmax <maximilian.rudolph@campus.tu-berlin.de>
2024-01-25 15:52:14 +0100

    lab-3 init

--- test/check_submission.sh
+++ test/check_submission.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 set -e
 
@@ -8,25 +8,32 @@ if [[ ! $# -eq 1 ]]; then
     exit 1
 fi
 
-TEST_SCRIPT="test/test_$1.py"
+TEST_SCRIPT="$(dirname $0)/test_$1.py"
 
 if [[ ! -e ${TEST_SCRIPT} ]]; then
-    echo "Requested test file ${TEST_SCRIPT} does not exist."
+    echo "Requested test file ${TEST_SCRIPT} does not exist!"
     exit 1
 fi
 
+echo "Packaging submission..."
 PACKAGING_DIR=$(mktemp -d)
-SOURCE_DIR=$(mktemp -d)
-BUILD_DIR=$(mktemp -d)
-
-echo "Packaging submission"
 cmake -B${PACKAGING_DIR}
 cmake --build ${PACKAGING_DIR} -t package_source
 
-echo "Extracting and building submission"
+echo "Extracting submission..."
+SOURCE_DIR=$(mktemp -d)
 (cd ${SOURCE_DIR} && find ${PACKAGING_DIR} -name '*.tar.gz' | head -n1 | xargs tar -xzf)
-cmake -S"$(find ${SOURCE_DIR} -maxdepth 1 -mindepth 1 -type d | head -n1)" -B${BUILD_DIR}
+EXTRACTED_SOURCE_DIR=$(find ${SOURCE_DIR} -maxdepth 1 -mindepth 1 -type d | head -n1)
+
+if [[ ! -e ${EXTRACTED_SOURCE_DIR}/group.txt ]]; then
+    echo "Submission does not contain a group.txt!"
+    exit 1
+fi
+
+echo "Building submission..."
+BUILD_DIR=$(mktemp -d)
+cmake -S"${EXTRACTED_SOURCE_DIR}" -B${BUILD_DIR}
 cmake --build ${BUILD_DIR} -t webserver
 
-echo "Executing tests"
-pytest --executable ${BUILD_DIR}/webserver ${TEST_SCRIPT}
+echo "Executing tests..."
+python3 -m pytest --executable ${BUILD_DIR}/webserver ${TEST_SCRIPT}
